2f40b785e8fd3d629a69beb2a7aef0117ce4e54d
samuditha jayawardena <54886813+samuditha97@users.noreply.github.com>
2022-01-30 13:56:13 +0530

    maked database connection

---
+++ app/src/main/java/com/example/klnresearch/test_show.java
@@ -0,0 +1,14 @@
+package com.example.klnresearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class test_show extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_show2);
+    }
+}
